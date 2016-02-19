82b50e6daa10ad60792455eccff4b634874b8197
starsheriff <jo@starsheriff.eu>
2016-02-19 22:07:40 +0100

    main.c compiles, added required include files to common

--- flashy/main.c
+++ flashy/main.c
@@ -2,21 +2,32 @@
 
 #define LED_2_PIN 5
 
+void delay(int n);
 
 int main(void)
 {
   // Enable GPIO clock on port A
   // RCC is of type RCC_TypeDef and defined and initiated in 
   // stm32f303xe.h
-  RCC->AHB1ENR |= RCC_AHBENR_GPIOAEN
-
-  GPIOx->BSRR = (uint32_t)GPIO_Pin;
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+  
+  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+  
+  GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5_0;
 
   while(1)
   {
-    BSP_LED_On(LED2);
-    HAL_Delay(100);
-    BSP_LED_Off(LED2);
-    HAL_Delay(200);
+    GPIOA->BSRR = GPIO_BSRR_BS_5;
+    delay(50000);
+
+    GPIOA->BRR = GPIO_BRR_BR_5;
+    delay(50000);
   }
+}
+
+void delay(int n) 
+{
+    while(n>0) {
+        n--;
+    }
 }
