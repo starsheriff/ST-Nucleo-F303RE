ee5b53cc319a9a8e765d3faf9d38ff6e803c490a
starsheriff <jo@starsheriff.eu>
2016-02-19 22:10:26 +0100

    main.c compiles, added required include files to common

--- first_run/main.c
+++ first_run/main.c
@@ -20,12 +20,14 @@
 #include "stm32f303xe.h"
  
 #define LED_PIN 5
+
 #define LED_ON() GPIOA->BSRRL |= (1 << 5)
 #define LED_OFF() GPIOA->BSRRH |= (1 << 5)
  
 int main() {
 	/* Enbale GPIOA clock */
 	// RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	
 	/* Configure GPIOA pin 5 as output */
 	//GPIOA->MODER |= (1 << (LED_PIN << 1));
 	/* Configure GPIOA pin 5 in max speed */
