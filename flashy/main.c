d9da5c75983929e0e8dd9919d1cbcdba1c4f79db
starsheriff <jo@starsheriff.eu>
2016-02-20 22:11:06 +0100

    updated link command, working build

--- flashy/main.c
+++ flashy/main.c
@@ -12,22 +12,34 @@ int main(void)
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
   
   GPIOA->MODER |= GPIO_MODER_MODER5_0;
+  //GPIOA->MODER |= (1 << (5 << 1));
   
-  GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5_0;
-
+  GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_5);
+  
+  GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR5;
+  
+  
   while(1)
   {
-    GPIOA->BSRR = GPIO_BSRR_BS_5;
-    delay(50000);
+    //GPIOA->BSRR = GPIO_BSRR_BS_5;
+    GPIOA->ODR = 0xFFFF;
+    delay(500000);
 
-    GPIOA->BRR = GPIO_BRR_BR_5;
-    delay(50000);
+    //GPIOA->BSRR = GPIO_BSRR_BR_5;
+    GPIOA->ODR = 0x0000;
+    delay(500000);
   }
+  
 }
 
 void delay(int n) 
 {
-    while(n>0) {
-        n--;
+    volatile int i,j;
+
+    for (i=0 ; i < n ; i++)
+    {
+        j++;
     }
+
+    return;
 }
